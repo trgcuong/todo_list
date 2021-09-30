dbea425dd0fcb0d284dee9e6abb2686ff68e6f16
cuongtruong <trgcuong@gmail.com>
2021-09-30 22:41:57 +0700

    refactor app_dialog

--- lib/utils/app_dialog.dart
+++ lib/utils/app_dialog.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/generated/l10n.dart';
 
-
-Future<void> showError({required BuildContext context, required dynamic error}) async {
+Future<void> showError({
+  required BuildContext context,
+  required dynamic error,
+}) async {
   return showDialog<void>(
     context: context,
     barrierDismissible: false,
     builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(S.of(context).error),
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              Text(error.toString()),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: Text(S.of(context).close),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
+      return AppAlertDialog(message: error.toString());
     },
   );
+}
+
+class AppAlertDialog extends StatelessWidget {
+  final String message;
+
+  const AppAlertDialog({Key? key, this.message = ''}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('S.of(context).error'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(message),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: Text('S.of(context).close'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
 }
